100b4680d2cf3d086ed07754f36db39f9799a05c
Federico <fede.toldo96@gmail.com>
2024-07-17 09:25:40 +0200

    test

--- functions.php
+++ functions.php
@@ -23,73 +23,10 @@ if (!function_exists('itas_setup')) :
 endif; // itas_setup
 add_action('after_setup_theme', 'itas_setup');
 
-
-if (!class_exists('Footer_Custom_Nav_Walker')) {
-  class Footer_Custom_Nav_Walker extends Walker_Nav_Menu
-  {
-    function start_el(&$output, $item, $depth = 0, $args = [], $id = 0)
-    {
-      $output .= "<li class='" .  implode(" ", is_array($item->classes) ? $item->classes : []) . "'>";
-      if ($item->url && $item->url != '#') {
-        $output .= '<a target="_blank" href="' . $item->url . '">';
-      } else {
-        $output .= '<span>';
-      }
-
-      if ($item->title === "Instagram") {
-        $output .= '<img src="' . get_template_directory_uri() . '/images/Instagram.svg" alt="' . $item->title . '" />';
-      } else if ($item->title === "Linkedin") {
-        $output .= '<img src="' . get_template_directory_uri() . '/images/Linkedin.svg" alt="' . $item->title . '" />';
-      } else if ($item->title === "Youtube") {
-        $output .= '<img src="' . get_template_directory_uri() . '/images/Youtube.svg" alt="' . $item->title . '" />';
-      }
-
-      if ($item->url && $item->url != '#') {
-        $output .= '</a>';
-      } else {
-        $output .= '</span>';
-      }
-    }
-  }
-}
-
-function add_theme_script(){
+function add_theme_script()
+{
   wp_enqueue_style('style', get_stylesheet_uri());
 
   wp_enqueue_script('script', get_template_directory_uri() . '/script.js', array('jquery'), 1, true);
 }
 add_action('wp_enqueue_scripts', 'add_theme_script');
-
-
-function create_post_type()
-{
-  register_post_type(
-    'projects',
-    // CPT Options
-    array(
-      'labels' => array(
-        'name' => __('Progetti'),
-        'singular_name' => __('Progetto')
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'rewrite' => array('slug' => 'projects'),
-      'show_in_rest' => true,
-    )
-  );
-
-  $supports = array('thumbnail', 'custom-fields', 'excerpt');
-  add_post_type_support('projects', $supports);
-}
-
-// Hooking up our function to theme setup
-add_action('init', 'create_post_type');
-
-
-function registering_custom_query_var($query_vars)
-{
-  $query_vars[] = 'pcat';
-  $query_vars[] = 'pyear';
-  return $query_vars;
-}
-add_filter('query_vars', 'registering_custom_query_var');
